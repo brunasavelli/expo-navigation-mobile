5c07c34fd68eddb5512a275663d13b52edefe0d3
Bruna Savelli <bnsavelli@gmail.com>
2025-03-26 23:05:39 -0300

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-// import Drawer2 from "./navigation/DrawerNavigator";
-// import Stack2 from "./navigation/StackNavigation.js";
-import Stack3 from "./navigation/TabNavigator";
+import Drawer2 from "./navigation/DrawerNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <Stack2 /> */}
-      {/* <Drawer2 /> */}
-      <Stack3 />
+      <Drawer2 />
     </NavigationContainer>
   );
 }
